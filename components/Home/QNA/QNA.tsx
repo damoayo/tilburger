7299c862666f19e95b56d20a3fdec226ea4e36c6
MJ JI <mamapai@naver.com>
2025-03-11 19:30:54 +0900

    확인커밋

--- components/Home/QNA/QNA.tsx
+++ components/Home/QNA/QNA.tsx
@@ -4,7 +4,7 @@ import AccordionUsage from "./AccordionUsage";
 const QNA = () => {
   return (
     <div>
-      <div className="w-[60%] mx-auto mt-20 mb-20  border-2border p-4 rounded-3xl bg-gray-50">
+      <div className="w-[60%] mx-auto mt-20 mb-20  border-2border pt-5 px-20 pb-20 rounded-3xl bg-gray-50">
         <AccordionUsage />
       </div>
     </div>
